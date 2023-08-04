0310e9ee632ab48ada809099bf193e7d969f91d0
pankoivan <panivan98@yandex.ru>
2023-08-04 23:03:45 +0300

    Внедрил все существующие репозитории для теста

--- src/main/java/org/forum/controllers/mvc/TestController.java
+++ src/main/java/org/forum/controllers/mvc/TestController.java
@@ -1,14 +1,34 @@
 package org.forum.controllers.mvc;
 
+import org.forum.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserDetailsRepository userDetailsRepository;
+
+    @Autowired
+    private SectionRepository sectionRepository;
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @Autowired
+    private AssignedRoleRepository assignedRoleRepository;
+
     @GetMapping("/")
     public String testReturn() {
-        System.out.println("Hello Word!");
+        System.out.println();
         return "index";
     }
 
