24f5fb7a85275b8c9bff8f0118aa2e9ec2737e32
pankoivan <panivan98@yandex.ru>
2023-08-11 14:05:09 +0300

    Добавил контроллер для главной страницы, а также контроллер для входа и регистрации

---
+++ src/main/java/org/forum/controllers/mvc/AuthController.java
@@ -0,0 +1,21 @@
+package org.forum.controllers.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String returnLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/registration")
+    public String returnRegistrationPage() {
+        return "registration";
+    }
+
+}
