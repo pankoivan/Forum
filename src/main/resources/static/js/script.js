e4294edc5deb006327f39bf010c9b2df246f0d89
pankoivan <panivan98@yandex.ru>
2023-08-19 11:45:30 +0300

    Сделал сообщение фрагментом. По аналогии далее фрагментами будут сделаны раздел и тема. Поправил разные файлы по мелочи

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -6,10 +6,10 @@ const likedStyle = "btn-success";
 for (let el of list) {
     el.addEventListener("click", () => {
         if (el.classList.contains(notLikedStyle)) {
-            el.className = "btn " + likedStyle;
+            el.className = "btn btn-sm " + likedStyle;
             el.getElementsByTagName("span")[1].textContent++;
         } else if (el.classList.contains(likedStyle)) {
-            el.className = "btn " + notLikedStyle;
+            el.className = "btn btn-sm " + notLikedStyle;
             el.getElementsByTagName("span")[1].textContent--;
         }
     });
