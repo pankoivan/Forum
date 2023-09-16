2d64da6cd5f2909facc592841fcd6f9d19bfb6b4
pankoivan <panivan98@yandex.ru>
2023-09-16 19:59:35 +0300

    Добавил сортировку для пользователей по аналогии со всеми другими страницами, где делал сортировку

--- src/main/java/org/forum/main/controllers/mvc/UsersController.java
+++ src/main/java/org/forum/main/controllers/mvc/UsersController.java
@@ -1,15 +1,21 @@
 package org.forum.main.controllers.mvc;
 
+import jakarta.servlet.http.HttpSession;
+import org.forum.auxiliary.sorting.enums.UserSortingProperties;
+import org.forum.auxiliary.sorting.options.UserSortingOption;
 import org.forum.main.controllers.mvc.common.ConvenientController;
+import org.forum.main.entities.Message;
+import org.forum.main.entities.User;
 import org.forum.main.services.interfaces.SectionService;
 import org.forum.main.services.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -33,13 +39,32 @@ public class UsersController extends ConvenientController {
     @GetMapping("/page{pageNumber}")
     public String returnUsersPage(Model model,
                                   Authentication authentication,
+                                  @SessionAttribute(value = "messageSortingOption", required = false)
+                                      UserSortingOption sortingOption,
                                   @PathVariable("pageNumber") Integer pageNumber) {
 
         addForHeader(model, authentication, sectionService);
-        add(model, "users", service.onPage(service.findAll(), pageNumber));
+        add(model, "users", sorted(sortingOption, pageNumber));
         add(model, "pagesCount", service.pagesCount(service.findAll()));
         add(model, "currentPage", pageNumber);
+
+        add(model, "sortingObject", service.emptySortingOption());
+        add(model, "properties", UserSortingProperties.values());
+        add(model, "directions", Sort.Direction.values());
+
         return "users";
+    }
+
+    @PostMapping("/page{pageNumber}/sort")
+    public String redirectCurrentPageAfterSorting(HttpSession session, UserSortingOption sortingOption) {
+        session.setAttribute("userSortingOption", sortingOption);
+        return "redirect:/users/page{pageNumber}";
+    }
+
+    private List<User> sorted(UserSortingOption sortingOption, Integer pageNumber) {
+        return sortingOption != null
+                ? service.onPage(service.findAllSorted(sortingOption), pageNumber)
+                : service.onPage(service.findAllSortedByDefault(), pageNumber);
     }
 
 }
