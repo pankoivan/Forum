8860e94076aada84af2e766c3430b6330584a390
pankoivan <panivan98@yandex.ru>
2023-09-21 18:27:23 +0300

    Добавил работу с ошибками 403, 404 и 405, добавил для этого отдельный специальный контроллер

---
+++ src/main/java/org/forum/main/controllers/mvc/interceptors/ErrorControllerImpl.java
@@ -0,0 +1,24 @@
+package org.forum.main.controllers.mvc.interceptors;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/error")
+public class ErrorControllerImpl implements ErrorController {
+
+    @RequestMapping
+    public String errors(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        return switch (status.toString()) {
+            case "403" -> "errors/403";
+            case "404" -> "errors/404";
+            case "405" -> "errors/405";
+            default -> "errors/unknown-error";
+        };
+    }
+
+}
